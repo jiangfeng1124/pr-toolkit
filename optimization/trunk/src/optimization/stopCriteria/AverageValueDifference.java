3a1f978932400a611d5fa758c79bfb8c803e9ae6
gracaninja@gmail.com <gracaninja@gmail.com@4abeb2da-2315-11df-909e-81cb1df1a2bf>
2010-08-23 10:16:17 +0000

    Add new stopping criteria

---
+++ optimization/trunk/src/optimization/stopCriteria/AverageValueDifference.java
@@ -0,0 +1,42 @@
+package optimization.stopCriteria;
+
+import optimization.gradientBasedMethods.Objective;
+
+public class AverageValueDifference implements StopingCriteria{
+	
+	/**
+	 * Stop if the different between values is smaller than a treshold
+	 */
+	protected double valueConvergenceValue=10E-6;
+	protected double previousValue = Double.NaN;
+	protected double currentValue = Double.NaN;
+	
+	public AverageValueDifference(double valueConvergenceValue){
+		this.valueConvergenceValue = valueConvergenceValue;
+	}
+	
+	public void reset(){
+		previousValue = Double.NaN;
+		currentValue = Double.NaN;
+	}
+
+	
+	public boolean stopOptimization(Objective obj){
+		if(Double.isNaN(currentValue)){
+			currentValue = obj.getValue();
+			return false;
+		}else {
+			previousValue = currentValue;
+			currentValue = obj.getValue();
+			double valueDiff = Math.abs(previousValue - currentValue);
+			double valueAverage = Math.abs(previousValue + currentValue)/2;	
+			if( valueDiff/valueAverage  < valueConvergenceValue){
+				System.out.println("Value Stop: "  
+						+ valueDiff + " avg: " + valueAverage  
+						+ " res: " + valueDiff/valueAverage);
+				return true;
+			}
+		}
+		return false;
+	}
+}
