dd5483e6f3762ff42e87aac0e087f85aa29732e8
kuzman.ganchev <kuzman.ganchev@4abeb2da-2315-11df-909e-81cb1df1a2bf>
2010-08-25 18:19:29 +0000

    added method to initialize a max ent HMM with random max-ent params.  Update to optimization for exception handling.

--- pr-toolkit/trunk/src/model/chain/hmm/HMMDirectGradientObjective.java
+++ pr-toolkit/trunk/src/model/chain/hmm/HMMDirectGradientObjective.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 import com.sun.tools.javac.comp.TransTypes;
 
+import constraints.CorpusConstraints;
+
 import model.AbstractCountTable;
 import model.AbstractSentenceDist;
 import model.CantNormalizeException;
@@ -22,21 +24,21 @@ import util.MathUtil;
 public class HMMDirectGradientObjective extends Objective {
 
 	AbstractCountTable counts;
-	public MultinomialMaxEntDirectGradientTrainer initTrainer, transitionTrainer, observationTrainer;
-	public int initOffset, transitionOffset, observationOffset;
+	MultinomialMaxEntDirectGradientTrainer initTrainer, transitionTrainer, observationTrainer;
+	int initOffset, transitionOffset, observationOffset;
 	public HMM model;
 	double gaussianPriorVariance;
 	AbstractSentenceDist[] sentenceDists;
 	int iter = 0;
 	double value = Double.NaN;
+	CorpusConstraints constraints;
 
 	MultinomialMaxEntDirectTrainerStats stats;
-
-
 	
-	public HMMDirectGradientObjective(HMM hmm, double prior, MultinomialMaxEntDirectTrainerStats stats){
+	public HMMDirectGradientObjective(HMM hmm, double prior, CorpusConstraints constraints, MultinomialMaxEntDirectTrainerStats stats){
 		model = hmm;
 		counts = hmm.getCountTable();
+		this.constraints = constraints;
 		initOffset = 0;
 		gaussianPriorVariance = prior;
 		sentenceDists = model.getSentenceDists();
@@ -47,10 +49,39 @@ public class HMMDirectGradientObjective extends Objective {
 		observationOffset = transitionOffset+transitionTrainer.numParams();
 		observationTrainer = new MultinomialMaxEntDirectGradientTrainer((MultinomialMaxEntTrainer) hmm.observationTrainer, hmmcounts.observationCounts);
 		parameters = new double[observationOffset+observationTrainer.numParams()];
-		gradient = new double[observationOffset+observationTrainer.numParams()];
 		this.stats = stats;
-
-
+		// the commented out code below is for debugging; it usually results in terrible performance
+		// and large gradients that cause the optimization to fail, so it's a good way to test
+		// fail-safes. 
+//		Random r = new Random(0);
+//		for (int i = 0; i < parameters.length; i++) {
+//			parameters[i] = r.nextDouble();
+//		}
+		// This is the better way to initialize:
+		// initialize parameters from first step of EM... 
+		if (constraints != null){
+			constraints.project(counts, sentenceDists, null, null);
+		} else {
+			counts.clear();
+			stats.beforeInference(this);
+			for(AbstractSentenceDist sd : sentenceDists){			
+				// sentenceEStep(sd, counts, stats);
+				stats.beforeSentenceInference(this,sd);
+				sd.initSentenceDist();
+				model.computePosteriors(sd);
+				model.addToCounts(sd,counts);	
+				stats.afterSentenceInference(this,sd);
+				sd.clearCaches();
+				sd.clearPosteriors();
+			}
+			stats.endInference(this);
+		}
+		initTrainer.getParametersForCounts(hmmcounts.initialCounts, parameters, initOffset);
+		transitionTrainer.getParametersForCounts(hmmcounts.transitionCounts, parameters, transitionOffset);
+		observationTrainer.getParametersForCounts(hmmcounts.observationCounts, parameters, observationOffset);
+		gradient = new double[parameters.length];
+		updateValueAndGradient();
+		System.out.println("Finished initializing "+this.getClass().getSimpleName()+" value: "+value+" ||grad||^2="+ArrayMath.twoNormSquared(gradient));
 //		testGradient();
 	}
 	
@@ -59,37 +90,28 @@ public class HMMDirectGradientObjective extends Objective {
 	 * @param r
 	 * @param jitter
 	 */
-	public void initializeRandom(Random r,double jitter){
+	public static void initializeRandom(HMM model, Random r,double jitter){
+		HMMCountTable hmmcounts = (HMMCountTable) model.getCountTable();
+		int initOffset = 0;
+		MultinomialMaxEntDirectGradientTrainer initTrainer = new MultinomialMaxEntDirectGradientTrainer((MultinomialMaxEntTrainer) model.initTrainer, hmmcounts.initialCounts);
+		int transitionOffset = initTrainer.numParams();
+		MultinomialMaxEntDirectGradientTrainer transitionTrainer = new MultinomialMaxEntDirectGradientTrainer((MultinomialMaxEntTrainer) model.transitionsTrainer, hmmcounts.transitionCounts);
+		int observationOffset = transitionOffset+transitionTrainer.numParams();
+		MultinomialMaxEntDirectGradientTrainer observationTrainer = new MultinomialMaxEntDirectGradientTrainer((MultinomialMaxEntTrainer) model.observationTrainer, hmmcounts.observationCounts);
+		double[] parameters = new double[observationOffset+observationTrainer.numParams()];
 		for(int i = 0; i < parameters.length; i++){
 			double rand = r.nextDouble()*2 -1; //Number between -1 and 1
 			double value =  jitter* rand;
 			parameters[i] = value;
 		}
+		initTrainer.setCountsAndParameters(hmmcounts.initialCounts, parameters, initOffset);
+		transitionTrainer.setCountsAndParameters(hmmcounts.transitionCounts, parameters, transitionOffset);
+		observationTrainer.setCountsAndParameters(hmmcounts.observationCounts, parameters, observationOffset);
+		initTrainer.getMultinomialAtCurrentParams(model.initialProbabilities);
+		transitionTrainer.getMultinomialAtCurrentParams(model.transitionProbabilities);
+		observationTrainer.getMultinomialAtCurrentParams(model.observationProbabilities);
 	}
-	
-	/**
-	 * Initialize from a initial E-Step
-	 * @param hmmcounts
-	 */
-	public void initializeFromFirstEstep(){
-		
-		counts.clear();
-		stats.beforeInference(this);
-		for(AbstractSentenceDist sd : sentenceDists){			
-			// sentenceEStep(sd, counts, stats);
-			stats.beforeSentenceInference(this,sd);
-			sd.initSentenceDist();
-			model.computePosteriors(sd);
-			model.addToCounts(sd,counts);	
-			stats.afterSentenceInference(this,sd);
-			sd.clearCaches();
-			sd.clearPosteriors();
-		}
-		stats.endInference(this);
-		initTrainer.getParametersForCounts(((HMMCountTable)counts).initialCounts, parameters, initOffset);
-		transitionTrainer.getParametersForCounts(((HMMCountTable)counts).transitionCounts, parameters, transitionOffset);
-		observationTrainer.getParametersForCounts(((HMMCountTable)counts).observationCounts, parameters, observationOffset);
-	}
+
 	
 	public void updateValueAndGradient(){
 		// set parameters: this.params -> trainer.params; trainer.params -> hmm.params
@@ -102,55 +124,74 @@ public class HMMDirectGradientObjective extends Objective {
 		observationTrainer.getMultinomialAtCurrentParams(model.observationProbabilities);
 
 		// compute the inference-induced counts at the current parameters.
-		counts.clear();
-		value = 0;
-		stats.beforeInference(this);
-		for(AbstractSentenceDist sd : sentenceDists){			
-			// sentenceEStep(sd, counts, stats);
-			stats.beforeSentenceInference(this,sd);
-			sd.initSentenceDist();
+		if (constraints != null){
+			// FIXME -- this is to test the idea. 
 			try{
-				model.computePosteriors(sd);
-				model.addToCounts(sd,counts);	
+			value = -constraints.project(counts, sentenceDists, null, null);
 			} catch (CantNormalizeException e){
-				System.err.print("Warning -- failed to normalize during update counts ("+e.problem+")");
-				System.err.println(" thrown at: "+e.getStackTrace()[0].toString());
-//				e.printStackTrace(System.err);
+				System.err.println("Warning -- failed to normalize projection of constraints. ");
+				System.err.println(e.getMessage());
+				System.err.println(e.getStackTrace().toString());
+				System.err.println("Assuming this is because parameters are too big/small");
 				System.err.println(String.format("max param = %.2f min param = %.2f; max grad = %.2f, min grad = %.2f, grad norm = %.2f",
 						MathUtil.max(parameters), MathUtil.min(parameters),
 						MathUtil.max(gradient), MathUtil.min(gradient),
 						Math.sqrt(ArrayMath.twoNormSquared(gradient))
-						));
-				TIntArrayList[] obs=model.observationProbabilities.getAvailableStates();
-				for (int state = 0; state < obs.length; state++) {
-					double max= Double.NEGATIVE_INFINITY;
-					double min = Double.POSITIVE_INFINITY;
-					int maxi=0, mini=0;
-					for (int word = 0; word < obs[state].size(); word++) {
-						double v = model.observationProbabilities.getCounts(state, obs[state].get(word));
-						if (v > max){
-							max = v;
-							maxi = word;
+				));
+				value = Double.POSITIVE_INFINITY;
+			}
+		} else {
+			counts.clear();
+			value = 0;
+			stats.beforeInference(this);
+			for(AbstractSentenceDist sd : sentenceDists){			
+				// sentenceEStep(sd, counts, stats);
+				stats.beforeSentenceInference(this,sd);
+				sd.initSentenceDist();
+				try{
+					model.computePosteriors(sd);
+					model.addToCounts(sd,counts);	
+					stats.afterSentenceInference(this,sd);
+				} catch (CantNormalizeException e){
+					System.err.print("Warning -- failed to normalize during update counts ("+e.problem+")");
+					System.err.println(" thrown at: "+e.getStackTrace()[0].toString());
+					//				e.printStackTrace(System.err);
+					System.err.println(String.format("max param = %.2f min param = %.2f; max grad = %.2f, min grad = %.2f, grad norm = %.2f",
+							MathUtil.max(parameters), MathUtil.min(parameters),
+							MathUtil.max(gradient), MathUtil.min(gradient),
+							Math.sqrt(ArrayMath.twoNormSquared(gradient))
+					));
+					TIntArrayList[] obs=model.observationProbabilities.getAvailableStates();
+					for (int state = 0; state < obs.length; state++) {
+						double max= Double.NEGATIVE_INFINITY;
+						double min = Double.POSITIVE_INFINITY;
+						int maxi=0, mini=0;
+						for (int word = 0; word < obs[state].size(); word++) {
+							double v = model.observationProbabilities.getCounts(state, obs[state].get(word));
+							if (v > max){
+								max = v;
+								maxi = word;
+							}
+							if (v < min){
+								min = v;
+								mini = word;
+							}
 						}
-						if (v < min){
-							min = v;
-							mini = word;
-						}
+						System.err.println(String.format(
+								" state = %3d  min=%f  max=%f (%s)  (minword = %s)" 
+								, state,min,max,model.corpus.getWordStrings(new int[]{obs[state].get(maxi)})[0]
+								, model.corpus.getWordStrings(new int[]{obs[state].get(mini)})[0]));
+						value = Double.POSITIVE_INFINITY;
 					}
-					System.err.println(String.format(
-							" state = %3d  min=%f  max=%f (%s)" 
-							, state,min,max,model.corpus.getWordStrings(new int[]{obs[state].get(maxi)})[0]));
-					value = Double.POSITIVE_INFINITY;
+					break;
 				}
-				break;
-			}		
-			value -= sd.getLogLikelihood();
-			stats.afterSentenceInference(this,sd);
-			sd.clearCaches();
-			sd.clearPosteriors();
-			
+				stats.endInference(this);
+				value -= sd.getLogLikelihood();
+				sd.clearCaches();
+				sd.clearPosteriors();
+
+			}
 		}
-		stats.endInference(this);
 		// FIXME: I don't like having to compute the value separately from the gradient
 		value +=  1/gaussianPriorVariance*ArrayMath.twoNormSquared(parameters);
 		// update the empirical counts so we can compute the value and gradient. 
@@ -176,69 +217,70 @@ public class HMMDirectGradientObjective extends Objective {
 //		System.out.println("------------");
 	}
 	
-	public void testGradient(){
-		System.out.println("testing gradient -- value = "+getValue());
-		System.out.println("Value (log-likely) = "+value);
-		System.out.print("Value (trainers) =   "+String.format("%.3f  ",
-				(initTrainer.getValue() + transitionTrainer.getValue() + observationTrainer.getValue())));
-		System.out.println(String.format("%.3f %.3f %.3f", 
-				initTrainer.getValue(), transitionTrainer.getValue(), observationTrainer.getValue()));
-		System.out.println("diff "+(value - (initTrainer.getValue() + transitionTrainer.getValue() + observationTrainer.getValue())));
-		TIntArrayList variables = new TIntArrayList();
-		int maxNumDirs = 100;
-		if (parameters.length > maxNumDirs) {
-			System.out.println("big gradient ("+parameters.length+")... subsampling directions");
-		}
-		Random rand = new Random(0);
-		for (int i = 0; i < gradient.length; i++) {
-			if (rand.nextDouble()*gradient.length <= maxNumDirs)
-				variables.add(i);
-		}
+	public void testGradient(){		
+	System.out.println("testing gradient -- value = "+getValue());
+	System.out.println("Value (log-likely) = "+value);
+	System.out.print("Value (trainers) =   "+String.format("%.3f  ",
+			(initTrainer.getValue() + transitionTrainer.getValue() + observationTrainer.getValue())));
+	System.out.println(String.format("%.3f %.3f %.3f", 
+			initTrainer.getValue(), transitionTrainer.getValue(), observationTrainer.getValue()));
+	System.out.println("diff "+(value - (initTrainer.getValue() + transitionTrainer.getValue() + observationTrainer.getValue())));
+	TIntArrayList variables = new TIntArrayList();
+	int maxNumDirs = 10;
+	if (parameters.length > maxNumDirs) {
+		System.out.println("big gradient ("+parameters.length+")... subsampling directions");
+	}
+	Random rand = new Random(0);
+	for (int i = 0; i < gradient.length; i++) {
+		if (rand.nextDouble()*gradient.length <= maxNumDirs)
+			variables.add(i);
+	}
 
-		double epsilon = 0.0001;
+	double epsilon = 0.00001;
+	updateValueAndGradient();
+	double origV = getValue();
+	double[] numGrad = new double[variables.size()];
+	for (int i = 0; i < numGrad.length; i++) {
+		numGrad[i] = gradient[variables.get(i)];
+	}
+	double[] plusGrad = new double[numGrad.length];
+	double[] minusGrad = new double[numGrad.length];
+	for (int i = 0; i < numGrad.length; i++) {
+		int gind = variables.get(i);
+		double theta = parameters[gind];
+		parameters[gind] += epsilon;
 		updateValueAndGradient();
-		double origV = getValue();
-		double[] numGrad = new double[variables.size()];
-		for (int i = 0; i < numGrad.length; i++) {
-			numGrad[i] = gradient[variables.get(i)];
-		}
-		double[] plusGrad = new double[numGrad.length];
-		double[] minusGrad = new double[numGrad.length];
-		for (int i = 0; i < numGrad.length; i++) {
+		plusGrad[i] = (getValue()-origV)/epsilon;
+		parameters[gind] = theta - epsilon;
+		updateValueAndGradient();
+		minusGrad[i] = (origV-getValue())/epsilon;
+		parameters[gind] = theta;
+	}
+	System.out.println("||analytical||^2="+util.ArrayMath.twoNormSquared(numGrad) + 
+			"  ||+||^2="+util.ArrayMath.twoNormSquared(plusGrad) +
+			"  ||-||^2="+util.ArrayMath.twoNormSquared(minusGrad) );
+	double plusCos = ArrayMath.cosine(plusGrad, numGrad);
+	double minusCos = ArrayMath.cosine(minusGrad, numGrad);
+	System.out.println("cos(+,comp) = "+plusCos);
+	System.out.println("cos(-,comp) = "+minusCos);
+	if (plusCos <=0.97 || minusCos <= 0.97){
+		System.out.println("index param analyt   +   -  feature");
+		for (int i = 0; i < minusGrad.length; i++) {
 			int gind = variables.get(i);
-			double theta = parameters[gind];
-			parameters[gind] += epsilon;
-			updateValueAndGradient();
-			plusGrad[i] = (getValue()-origV)/epsilon;
-			parameters[gind] = theta - epsilon;
-			updateValueAndGradient();
-			minusGrad[i] = (origV-getValue())/epsilon;
-			parameters[gind] = theta;
+			String r = null;
+			if(gind>=initOffset && gind<transitionOffset) r = " init "+initTrainer.featureToString(gind-initOffset);
+			if(gind>=transitionOffset && gind<observationOffset) r = "trans "+transitionTrainer.featureToString(gind-transitionOffset);
+			if(gind>=observationOffset) r = "  obs "+observationTrainer.featureToString(gind-observationOffset);
+			System.out.print(String.format("%2d  ", gind));
+			System.out.print(myf2str(parameters[i]));
+			System.out.print(myf2str(numGrad[i]));
+			System.out.print(myf2str(plusGrad[i]));
+			System.out.print(myf2str(minusGrad[i]));
+			System.out.println(String.format("%s", r));
 		}
-		System.out.println("||analytical||^2="+util.ArrayMath.twoNormSquared(numGrad) + 
-				"  ||+||^2="+util.ArrayMath.twoNormSquared(plusGrad) +
-				"  ||-||^2="+util.ArrayMath.twoNormSquared(minusGrad) );
-		double plusCos = ArrayMath.cosine(plusGrad, numGrad);
-		double minusCos = ArrayMath.cosine(minusGrad, numGrad);
-		System.out.println("cos(+,comp) = "+plusCos);
-		System.out.println("cos(-,comp) = "+minusCos);
-		if (plusCos <=0.97 || minusCos <= 0.97){
-			for (int i = 0; i < minusGrad.length; i++) {
-				String r = null;
-				if(i>=initOffset && i<transitionOffset) r = " init "+initTrainer.featureToString(i-initOffset);
-				if(i>=transitionOffset && i<observationOffset) r = "trans "+transitionTrainer.featureToString(i-transitionOffset);
-				if(i>=observationOffset) r = "  obs "+observationTrainer.featureToString(i-observationOffset);
-				System.out.print(String.format("%2d  ", i));
-				System.out.print(myf2str(parameters[i]));
-				System.out.print(myf2str(numGrad[i]));
-				System.out.print(myf2str(plusGrad[i]));
-				System.out.print(myf2str(minusGrad[i]));
-				System.out.println(String.format("%s", r));
-			}
-			//throw new AssertionError("probable bug in gradient computation!");
-		}
-		
-		updateValueAndGradient();
+		//throw new AssertionError("probable bug in gradient computation!");
+	}	
+	updateValueAndGradient();
 	}
 	
 	private String myf2str(double f){
